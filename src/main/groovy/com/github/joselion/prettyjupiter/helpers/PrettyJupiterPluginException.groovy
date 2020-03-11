82768428942eaf921640f244bfd8180db5920f7a
JoseLion <joseluis5000l@gmail.com>
2020-03-11 00:49:27 -0500

    Fix plugin precedence
    - Make plugin configuration lazy. Only set if `JavaTest` plugin type is invoked

--- src/main/groovy/com/github/joselion/prettyjupiter/helpers/PrettyJupiterPluginException.groovy
+++
@@ -1,8 +0,0 @@
-package com.github.joselion.prettyjupiter.helpers
-
-public class PrettyJupiterPluginException extends RuntimeException {
-
-  public PrettyJupiterPluginException(String message, Throwable cause) {
-    super(message, cause)
-  }
-}
